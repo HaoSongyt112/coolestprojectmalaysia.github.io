72bf7553792fe065aeb0aa8bc417274988144682
HaoSongyt112 <haosong0021@gmail.com>
2024-09-15 00:09:05 +0800

    Add files via upload

---
+++ home/page1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#detail").click(function(){
+      $("#more").slideToggle("fast");
+    });
+  });
+
+$(document).ready(function(){
+    $("#Vaquita").click(function(){
+      $("#Vaquita2").slideToggle("fast");
+    });
+  });
